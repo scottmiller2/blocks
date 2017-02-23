a0572ffd032baf9c27332cfa66c4b36512898445
scottmiller2 <scott.miller2@stvincent.edu>
2017-02-23 01:57:45 -0500

    Matching working, menus working, timer working — counter not working on top,

--- blocks1/ViewController.swift
+++ blocks1/ViewController.swift
@@ -11,9 +11,7 @@ import GameKit
 
 class ViewController: UIViewController, UICollectionViewDelegate {
 
-    @IBOutlet weak var timerLabel: UILabel!
     @IBOutlet weak var counterLabel: UILabel!
-    @IBOutlet weak var blocksTopBar: UILabel!
     
     
     var colorsRed: [Double] = [1.0, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.9, /*8*/ 1.0, 1.0, 0.2, 0.6, 0.5, 0.8, 0.9, 0.6, 0.0, 0.9]
@@ -24,6 +22,17 @@ class ViewController: UIViewController, UICollectionViewDelegate {
     
     var myBlocks = [UIImageView]()
     var myCircles = [UIImageView]()
+    
+    var menuView = UIView()
+    var menuLabel = UILabel()
+    var menuPlay = UIButton()
+    var menuHelp = UIButton()
+    var postGameView = UIView()
+    var postGamePlayButton = UIButton()
+    var scoreLabel = UILabel()
+    var timerLabel = UILabel()
+    var gameTopTitle = UILabel()
+    var gameTopCounter = UILabel()
     
     //counting
     var moveCounter = 0
@@ -47,46 +56,256 @@ class ViewController: UIViewController, UICollectionViewDelegate {
     var xMatchTag = 0
     var yMatchTag = 0
     
-    func setupGame()  {
+    /** Menu **/
+    
+    func preGame() {
+        
+         for x in myCircles {
+         x.removeFromSuperview()
+         }
+        
+        //Reset counting variables
+        arrayCounter = 0
+        moveCounter = 0
+        matchCounter = 0
+        
+        
+        //Configure Labels
         timerLabel.isHidden = false
-        counterLabel.isHidden = false
-        blocksTopBar.isHidden = false
         
-        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(ViewController.subtractTime), userInfo: nil, repeats: true)
-    }
-    
-    //Step through circle array and match tint colors to match tags
+        //Set view background
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "menuBG.jpg")!)
+        
+        //Build the menu box
+        menuView = UIView(frame: CGRect(x: 0, y: 0, width: 270, height: 270))
+        menuView.backgroundColor = UIColor(patternImage: UIImage(named: "background.png")!)
+        self.view.addSubview(menuView)
+        menuView.layer.zPosition = 1;
+        //menuView.layer.cornerRadius = 10
+        menuView.isHidden = false
+        
+        //Menu shadow
+        menuView.layer.shadowColor = UIColor.black.cgColor
+        menuView.layer.shadowOpacity = 0.7
+        menuView.layer.shadowOffset = CGSize.zero
+        menuView.layer.shadowRadius = 15
+        
+        //Center the menu
+        menuView.center = self.view.center
+        menuView.center.x = self.view.center.x
+        menuView.center.y = self.view.center.y
+        
+        //Menu label (Blocks title)
+        menuLabel = UILabel(frame: CGRect(x: 25, y: 25, width: 200, height: 50))
+        menuLabel.font = UIFont(name: "Helvetica", size: 45.0)
+        menuLabel.center = CGPoint(x: 160, y: 285)
+        menuLabel.textAlignment = .center
+        menuLabel.text = "Blocks"
+        menuLabel.layer.zPosition = 1;
+        menuLabel.textColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
+        self.view.addSubview(menuLabel)
+        
+        //Blocks title shadow
+        menuLabel.layer.shadowColor = UIColor.black.cgColor
+        menuLabel.layer.shadowOpacity = 0.7
+        menuLabel.layer.shadowOffset = CGSize.zero
+        menuLabel.layer.shadowRadius = 4
+        
+        menuLabel.center = self.view.center
+        menuLabel.center.x = self.view.center.x
+        menuLabel.center.y = self.view.center.y - 60
+        
+        //Menu play button
+        menuPlay = UIButton(frame: CGRect(x: 25, y: 25, width: 270, height: 65))
+        menuPlay.backgroundColor = UIColor(red: 0.3, green: 0.7, blue: 0.9, alpha: 0.9)
+        //menuPlay.titleLabel?.font = UIFont.init(name: "Helvetica", size:30)
+        menuPlay.titleLabel!.font =  UIFont(name: "Helvetica", size: 40)
+        menuPlay.setTitle("PLAY", for: .normal)
+        menuPlay.addTarget(self, action:#selector(self.menuPlayButtonClicked), for: .touchUpInside)
+        self.view.addSubview(menuPlay)
+        menuPlay.layer.zPosition = 1;
+        
+        
+        menuPlay.center = self.view.center
+        menuPlay.center.x = self.view.center.x
+        menuPlay.center.y = self.view.center.y + 40
+        
+        //Menu help button
+        menuHelp = UIButton(frame: CGRect(x: 25, y: 25, width: 270, height: 65))
+        menuHelp.backgroundColor = UIColor(red: 0.1, green: 0.4, blue: 0.8, alpha: 0.9)
+        menuHelp.titleLabel!.font =  UIFont(name: "Helvetica", size: 40)
+        menuHelp.setTitle("HELP", for: .normal)
+        menuHelp.addTarget(self, action:#selector(self.menuHelpButtonClicked), for: .touchUpInside)
+        self.view.addSubview(menuHelp)
+        menuHelp.layer.zPosition = 1;
+        
+        
+        menuHelp.center = self.view.center
+        menuHelp.center.x = self.view.center.x
+        menuHelp.center.y = self.view.center.y + 105
+        
+        //Game top title (middle)
+        gameTopTitle = UILabel(frame: CGRect(x: 25, y: 25, width: 200, height: 50))
+        gameTopTitle.font = UIFont(name: "Helvetica", size: 40)
+        gameTopTitle.center = CGPoint(x: 160, y: 285)
+        gameTopTitle.textAlignment = .center
+        gameTopTitle.text = "Blocks"
+        gameTopTitle.layer.zPosition = 1;
+        gameTopTitle.textColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
+        self.view.addSubview(gameTopTitle)
+        
+        gameTopTitle.center = self.view.center
+        gameTopTitle.center.x = self.view.center.x
+        gameTopTitle.center.y = self.view.center.y - 335
+        
+        //Game top counter (right side)
+        gameTopCounter = UILabel(frame: CGRect(x: 25, y: 25, width: 200, height: 50))
+        gameTopCounter.font = UIFont(name: "Helvetica", size: 35)
+        gameTopCounter.center = CGPoint(x: 160, y: 285)
+        gameTopCounter.textAlignment = .center
+        gameTopCounter.text = "\(moveCounter)"
+        gameTopCounter.layer.zPosition = 1;
+        gameTopCounter.textColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
+        self.view.addSubview(gameTopCounter)
+        gameTopCounter.isHidden = true
+        
+        gameTopCounter.center = self.view.center
+        gameTopCounter.center.x = self.view.center.x + 150
+        gameTopCounter.center.y = self.view.center.y - 335
 
-    func setTags() {
-        var tagCount = 0
+        
+        print("In preGame")
+    }
+
+    func postGame(){
+        print("In Post Game")
+        timerLabel.removeFromSuperview()
+        
         for x in myBlocks {
-            x.tag = tagCount
-            tagCount += 1
+            x.removeFromSuperview()
         }
-    }
-
-    func checkMatch(){
         
-        for _ in myBlocks {
-        if (100 / 2 > sqrt((pos1.x - pos2.x) * (pos1.x - pos2.x) + (pos1.y - pos2.y) * (pos1.y - pos2.y))) {
-            
-            print("match made in checkMatch")
-            myBlocks[objectDragging].isHidden = true
-            myCircles[circleLocation].tintColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
-            }
-        }
+        gameTopTitle.isHidden = true
+        gameTopCounter.isHidden = true
+        
+        timer.invalidate()
+        
+        //Build the menu box
+        postGameView = UIView(frame: CGRect(x: 0, y: 0, width: 270, height: 270))
+        postGameView.backgroundColor = UIColor(patternImage: UIImage(named: "background.png")!)
+        self.view.addSubview(postGameView)
+        postGameView.layer.zPosition = 1;
+        postGameView.isHidden = false
+        
+        //Menu shadow
+        postGameView.layer.shadowColor = UIColor.black.cgColor
+        postGameView.layer.shadowOpacity = 0.7
+        postGameView.layer.shadowOffset = CGSize.zero
+        postGameView.layer.shadowRadius = 12
+        
+        //Center the menu
+        postGameView.center = self.view.center
+        postGameView.center.x = self.view.center.x
+        postGameView.center.y = self.view.center.y
+        
+        //Blocks title
+        menuLabel = UILabel(frame: CGRect(x: 25, y: 25, width: 200, height: 55))
+        menuLabel.font = UIFont(name: "Helvetica", size: 40)
+        menuLabel.center = CGPoint(x: 160, y: 285)
+        menuLabel.textAlignment = .center
+        menuLabel.text = "Blocks"
+        menuLabel.layer.zPosition = 1;
+        menuLabel.textColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
+        self.view.addSubview(menuLabel)
+        
+        //Blocks title shadow
+        menuLabel.layer.shadowColor = UIColor.black.cgColor
+        menuLabel.layer.shadowOpacity = 0.7
+        menuLabel.layer.shadowOffset = CGSize.zero
+        menuLabel.layer.shadowRadius = 4
+        
+        menuLabel.center = self.view.center
+        menuLabel.center.x = self.view.center.x
+        menuLabel.center.y = self.view.center.y - 80
+        
+        //Score label
+        scoreLabel = UILabel(frame: CGRect(x: 25, y: 25, width: 200, height: 50))
+        scoreLabel.font = UIFont(name: "Helvetica", size: 25.0)
+        scoreLabel.center = CGPoint(x: 160, y: 285)
+        scoreLabel.textAlignment = .center
+        scoreLabel.text = "Moves: \(moveCounter)"
+        scoreLabel.layer.zPosition = 1;
+        scoreLabel.textColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
+        self.view.addSubview(scoreLabel)
+        
+        scoreLabel.center = self.view.center
+        scoreLabel.center.x = self.view.center.x
+        scoreLabel.center.y = self.view.center.y - 13
+        
+        //Post game play button
+        postGamePlayButton = UIButton(frame: CGRect(x: 25, y: 25, width: 270, height: 65))
+        postGamePlayButton.backgroundColor = UIColor(red: 0.2, green: 0.6, blue: 0.8, alpha: 0.9)
+        postGamePlayButton.setTitle("Play Again", for: .normal)
+        postGamePlayButton.titleLabel!.font =  UIFont(name: "Helvetica", size: 40)
+        postGamePlayButton.addTarget(self, action:#selector(self.postGamePlayButtonClicked), for: .touchUpInside)
+        self.view.addSubview(postGamePlayButton)
+        postGamePlayButton.layer.zPosition = 1;
+        
+        postGamePlayButton.center = self.view.center
+        postGamePlayButton.center.x = self.view.center.x
+        postGamePlayButton.center.y = self.view.center.y + 40
+        
+        //Post game help button
+        menuHelp = UIButton(frame: CGRect(x: 25, y: 25, width: 270, height: 65))
+        menuHelp.backgroundColor = UIColor(red: 0.0, green: 0.3, blue: 0.7, alpha: 0.9)
+        menuHelp.titleLabel!.font =  UIFont(name: "Helvetica", size: 40)
+        menuHelp.setTitle("Help", for: .normal)
+        menuHelp.addTarget(self, action:#selector(self.menuHelpButtonClicked), for: .touchUpInside)
+        self.view.addSubview(menuHelp)
+        menuHelp.layer.zPosition = 1;
+        
+        
+        menuHelp.center = self.view.center
+        menuHelp.center.x = self.view.center.x
+        menuHelp.center.y = self.view.center.y + 105
 
+        
     }
-    
-    override func viewDidLoad() {
+    func setupGame()  {
+        
+        print("In setupGame")
+        
+        arrayCounter = 0
+        matchCounter = 0
+        moveCounter = 0
+        seconds = 60
+        
+        gameTopCounter.isHidden = false
+        
+        //Timer label
+        timerLabel = UILabel(frame: CGRect(x: 25, y: 25, width: 200, height: 50))
+        timerLabel.font = UIFont(name: "Helvetica", size: 35.0)
+        timerLabel.center = CGPoint(x: 160, y: 285)
+        timerLabel.textAlignment = .center
+        timerLabel.text = "\(seconds)"
+        timerLabel.layer.zPosition = 1;
+        timerLabel.textColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
+        self.view.addSubview(timerLabel)
+        
+        timerLabel.center = self.view.center
+        timerLabel.center.x = self.view.center.x - 150
+        timerLabel.center.y = self.view.center.y - 335
+        
+        gameTopTitle.isHidden = false
+        
         //Shuffle the array
         var shuffledArray: [Int] = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: orderArray) as! [Int]
-
+        
         
         //Reorder the shuffled array so the two white circles are in index 8 and 9
         for a in 0...17 {
             if shuffledArray[a] == 8 {
-            let element = shuffledArray[8]
+                let element = shuffledArray[8]
                 shuffledArray[a] = element
                 shuffledArray[8] = 8
             }
@@ -98,9 +317,6 @@ class ViewController: UIViewController, UICollectionViewDelegate {
         }
         
         //Label and background
-        timerLabel.isHidden = true
-        counterLabel.isHidden = true
-        blocksTopBar.isHidden = true
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background.png")!)
         
         //Setup for block and circle building
@@ -117,7 +333,7 @@ class ViewController: UIViewController, UICollectionViewDelegate {
             for j in 1...6 {
                 
                 let randArrayVar = shuffledArray[nextPos]
-
+                
                 
                 //circles
                 
@@ -145,9 +361,9 @@ class ViewController: UIViewController, UICollectionViewDelegate {
                 squareImg.center.y = CGFloat(initY + j * (imgWidth + padding))
                 
                 myBlocks.append(squareImg)
-                    
+                
                 squareImg.isUserInteractionEnabled = true
-                    
+                
                 squareImg.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(ViewController.handlePan(_:))))
                 
                 view.addSubview(squareImg)
@@ -163,20 +379,62 @@ class ViewController: UIViewController, UICollectionViewDelegate {
         
         //Call to set the tags for the circles and blocks on the screen
         setTags()
-
+        
         //******* Debugging ********//
         //Print out the index and tags, as well as tintColors
         for y in 0...17{
-        print("index \(y)")
-        print("BLOCK tag \(myBlocks[y].tag)")
-        print("block color \(myBlocks[y].tintColor)")
-        print("CIRCLE tag \(myCircles[y].tag)")
-        print("circle color \(myCircles[y].tintColor)")
-        print("")
+            print("index \(y)")
+            print("BLOCK tag \(myBlocks[y].tag)")
+            print("block color \(myBlocks[y].tintColor)")
+            print("CIRCLE tag \(myCircles[y].tag)")
+            print("circle color \(myCircles[y].tintColor)")
+            print("")
         }
         
-        
         /* Check here for initial game load matches? */
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(ViewController.subtractTime), userInfo: nil, repeats: true)
+    }
+
+    
+    func menuPlayButtonClicked() {
+        print("Play Button Clicked")
+        menuView.isHidden = true
+        menuPlay.isHidden = true
+        menuLabel.isHidden = true
+        menuHelp.isHidden = true
+        timerLabel.isHidden = false
+        
+        setupGame()
+    }
+    func menuHelpButtonClicked() {
+        print("Help Button Clicked")
+        UIApplication.shared.openURL(NSURL(string: "http://scomiller.com/game")! as URL)
+    }
+    func postGamePlayButtonClicked() {
+        print("Post game play button clicked")
+        timerLabel.removeFromSuperview()
+        postGameView.isHidden = true
+        postGamePlayButton.isHidden = true
+        menuLabel.isHidden = true
+        scoreLabel.isHidden = true
+        menuHelp.isHidden = true
+        
+        setupGame()
+    }
+    
+    //Step through circle array and match tint colors to match tags
+
+    func setTags() {
+        var tagCount = 0
+        for x in myBlocks {
+            x.tag = tagCount
+            tagCount += 1
+        }
+    }
+    
+    override func viewDidLoad() {
+        preGame()
     }
     
     //**** Handle the movements ***** //
@@ -210,8 +468,8 @@ class ViewController: UIViewController, UICollectionViewDelegate {
                 matchCounter += 1
                 
                 print("Match Count: \(matchCounter)")
-                if matchCounter == 17 {
-                    print("All matches made")
+                if matchCounter == 16 || seconds == 0 {
+                postGame()
                 }
             }
             else {
@@ -241,6 +499,7 @@ class ViewController: UIViewController, UICollectionViewDelegate {
         
         if(seconds == 0)  {
             timer.invalidate()
+            postGame()
         }
         else if (seconds <= 10) {
             timerLabel.textColor = UIColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 1.0)
